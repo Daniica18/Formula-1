4d28b8a3965978ec74ac8d159f19183603cc1f7f
TatjanaStojanovic1701 <tatjana.stojanovic1701@gmail.com>
2025-05-19 15:02:35 +0200

    Add Drivers

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -1,8 +1,59 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+//import Loader from "./Loader";
+import { useNavigate } from "react-router";
+
 export default function Drivers () {
+   const[drivers,setDrivers] = useState([]);
+  // const[loading,setLoading] = useState(true);
+   const navigate = useNavigate();
+    
+   
+    useEffect(()=> {
+        getDrivers();
 
-     return (
-        <>
-        </>
-     );
+    },[]);
 
+     const getDrivers = async ()=>{
+        const url = 'http://ergast.com/api/f1/2013/driverStandings.json';
+        const response = await axios.get(url);
+        console.log(response);
+        setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+       
+        
+        //setLoading(false);
+
+
+    };
+    
+        return(
+         <div>
+        <table>
+      <thead>
+        <tr>
+          <th>Drivers Chamionship Standing</th>
+         
+        </tr>
+      </thead>
+      <tbody>
+       {drivers.map((driver,i) =>{
+         
+          return(
+           <tr key={driver.Driver.driverId}>
+            <td>{driver.position}</td>
+            <td>{driver.Driver.familyName} {driver.Driver.givenName}</td>
+            <td>{driver.Constructors[0].name}</td>
+            <td>{driver.points}</td>
+
+           </tr>
+          )
+
+        })}
+         
+       
+      </tbody>
+    </table>
+         </div>
+
+    )
 }
