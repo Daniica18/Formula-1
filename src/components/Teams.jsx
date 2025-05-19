3805dbf6630fa7c0c825a15ae828d49e701cc512
Biljana111 <b.maricns@gmail.com>
2025-05-19 15:57:11 +0200

    Add second part of Teams.jsx

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -30,10 +30,23 @@ export default function Teams() {
 
     return (
         <div>
-         {teams.map((team)=>{
+         {teams.map((team,i)=>{
             console.log(team);
             return(
                 <table>
+                    <thead>
+                        <tr>
+                            <th>Constructors Standings</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr key={team.}> 
+                            <td>{team.ConstructorStandings[0].position}</td>
+                            <td>{team.ConstructorStandings[0].Constructor.constructorId}</td>
+                            <td><a href="Details"/></td>
+                            <td>{team.ConstructorStandings[0].points}</td>
+                        </tr>
+                    </tbody>
                    
 
                 </table>
