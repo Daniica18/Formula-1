2fb4c080762d42f3f0da5e84dfe2c0ffa8b0e32c
aleksa94jok <aleksa.jokanovic94@outlook.com>
2025-05-20 15:32:26 +0200

    Made functional Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Home from "./components/Home"
 import Drivers from "./components/Drivers";
 import Teams from "./components/Teams";
 import Races from "./components/Races";
@@ -7,6 +10,28 @@ import TeamsDetails from "./components/TeamsDetails";
 import RacesDetails from "./components/RacesDetails";
 
 export default function App() {
+    const [stateFlags, setStateFlags] = useState([]);
+
+    useEffect(() => {
+        getStateFlags();
+    }, []);
+
+    const getStateFlags = async () => {
+        const url = `https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json`;
+        const response = await axios.get(url);
+        setStateFlags(response.data);
+        console.log(response.data);
+    };
+
+    // const filteredData = stateFlags.filter((el) => {
+    
+    // if (text === "" || region === "") {
+    
+    // return el;
+    
+    // }
+//});
+
     return (
         <Router>
             <nav className="main-navigation">
@@ -23,8 +48,8 @@ export default function App() {
                 </ul>
             </nav>
 
-            <Routes>
-                <Route path="/" element={<Drivers />} />
+            <Routes>                
+                <Route path="/" element={<Home />} />
                 <Route path="/drivers" element={<Drivers />} />
                 <Route path="/teams" element={<Teams />} />
                 <Route path="/races" element={<Races />} />
