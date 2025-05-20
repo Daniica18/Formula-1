8de7689283f7287abb4721a9fc9e5efa5b18ba63
aleksa94jok <aleksa.jokanovic94@outlook.com>
2025-05-20 08:31:13 +0200

    loader, Races format

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-// import Loader from "./Loader";
+import Loader from "./Loader";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
@@ -17,42 +17,41 @@ export default function Teams() {
     }, []);
 
     const getTeams = async () => {
-        const url ="http://ergast.com/api/f1/2013/constructorStandings.json";
+        const url = "http://ergast.com/api/f1/2013/constructorStandings.json";
         const response = await axios.get(url);
-        console.log(response.data.MRData.StandingsTable.StandingsLists);
-        setTeams(response.data.MRData.StandingsTable.StandingsLists);
+        console.log(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+        setTeams(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
         setIsLoading(false);
     }
 
-    // if(isLoading){
-    //     return<Loader />
-    // }
+    if(isLoading){
+        return<Loader />
+    }
 
     return (
         <div>
-         {teams.map((team,i)=>{
-            console.log(team);
-            return(
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Constructors Standings</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr key={team.}> 
-                            <td>{team.ConstructorStandings[0].position}</td>
-                            <td>{team.ConstructorStandings[0].Constructor.constructorId}</td>
-                            <td><a href="Details"/></td>
-                            <td>{team.ConstructorStandings[0].points}</td>
-                        </tr>
-                    </tbody>
-                   
+            <table>
+                <thead>
+                    <tr>
+                        <th>Constructors Standings</th>
+                    </tr>
+                </thead>
+                {teams.map((team) => {
+                    return (
+                        <tbody>
+                            <tr key={team.Constructor.constructorId}>
+                                <td>{team.position}</td>
+                                <td>{team.Constructor.name}</td>
+                                <td><a href="Details" /></td>
+                                <td>{team.points}</td>
+                            </tr>
+                        </tbody>
 
-                </table>
-            )
-         })}   
-            
+
+                    )
+                })}
+            </table>
+
         </div>
     );
 
