f4c942cb272a117814fa68473a73a2fc608d7d70
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-20 08:59:37 +0200

    Modified App

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import Drivers from "./components/Drivers";
 import Teams from "./components/Teams";
 import Races from "./components/Races";
 
-export default function App () {
-  return (
-      <Router>
+export default function App() {
+    return (
+        <Router>
             <nav className="main-navigation">
                 <ul>
                     <li>
                         <Link to="/">Drivers</Link>
                     </li>
-                       <li>
+                    <li>
                         <Link to="/teams">Teams</Link>
                     </li>
-                     <li>
+                    <li>
                         <Link to="/races">Races</Link>
                     </li>
                 </ul>
@@ -24,9 +24,12 @@ export default function App () {
                 <Route path="/" element={< Drivers />} />
                 <Route path="/teams" element={< Teams />} />
                 <Route path="/races" element={< Races />} />
-                {/* <Route path="/details/:id" element={<CommentDetails />} /> */}
+                <Route path="/details/:id" element={<DriversDetails />} />
+                <Route path="/teams/details/:id" element={<TeamsDetails />} />
+                <Route path="/races/details/:id" element={<RacesDetails />} />
+
             </Routes>
         </Router>
-  
-  )
+
+    )
 };
