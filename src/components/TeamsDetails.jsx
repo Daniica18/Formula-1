6fd2fa6cdda7d304994a0db24399acde1384c031
markobrajkovic <marko.i.brajkovic@gmail.com>
2025-05-20 10:11:10 +0200

    Add first part of TeamsDetails.jsx

--- src/components/TeamsDetails.jsx
+++ src/components/TeamsDetails.jsx
@@ -1,6 +1,36 @@
+import axios from "axios";
+import { useEffect, useState} from "react";
+import {useParams} from "react-router";
+ import Loader from"./Loader";
+
+
+
+
 export default function TeamsDetails() {
+    
+     const[teamsDetails, setTeamsDetails] = useState({});
+    const[isLoading, setIsLoading] = useState(true);
+    const params = useParams();
+
+    useEffect(()=>{
+        getTeamsDetails();
+    },[])
+
+    const getTeamsDetails = async () =>{
+        const url ="http://ergast.com/api/f1/2013/constructors/id/constructorStandings.json";
+        const response = await axios.get(url);
+        console.log(response);
+        setTeamsDetails(response.data);
+        setIsLoading(false);
+    };
+
+    if(isLoading){
+         return(<Loader/>)
+     }
+    
     return (
         <>
+        
         </>
 
     )
