dfa7b4e2a08dc9fdddf7473aaf7464c2a4def307
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-21 10:55:19 +0200

    Modified Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
-import React from "react"
-export default function Home() {
-    return (
-        <div className="homeDiv">
-            <img src={`/public/img/F1-2013-Legends-Edition.jpg`} alt="background_img"
-                style={{ width: '100%', height: 'auto' }} />
-        </div>
-    )
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
+    
