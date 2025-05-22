bad4c34d2c23e8cbf612329888450db461a55d18
Biljana111 <b.maricns@gmail.com>
2025-05-22 13:33:24 +0200

    Add changes to contact

--- src/components/CardThree.jsx
+++ src/components/CardThree.jsx
@@ -3,13 +3,14 @@ export default function CardThree() {
     <>
       <div className="card3">
         <h1>TECH WEEKLY: McLaren’s ingenious design is the latest step in a spectacular evolution in rear brake duct design</h1>
-
+        <br/>
         <img src="../public/img/formulacard3.jpg" style={{ width: "950px", height: "600px" }} />
         <p><b>There has been a lot of intrigue recently about how an ingenious McLaren rear brake duct design is helping the MCL39 have better tyre temperature control than the others. At circuits which induce particularly high rear tyre temperatures, it has helped amplify McLaren’s advantage
         </b>
           <br />
           <br />
           Although the exact way the duct is configured internally is not yet widely known, the FIA has checked and confirmed that the design is in full compliance with the regulations and that it has no concerns about it.
+          <br />
           <br />
           
 
