8aee7325a55074f0eed28e7125f1b58c6bae5ab7
TatjanaStojanovic1701 <tatjana.stojanovic1701@gmail.com>
2025-05-23 10:15:35 +0200

    Modified cards

--- src/components/CardFour.jsx
+++ src/components/CardFour.jsx
@@ -7,14 +7,14 @@ export default function CardFour() {
 
         <br /><br />
 
-        <img src="../public/img/Card4.new.jpg" style={{width:'1000px'}}></img>
+        <img src="../public/img/Card4.new.jpg" style={{ width: '1000px' }}></img>
 
         <br /><br />
 
         <p>
-          <b>McLaren may well have the fastest overall package this year, but Max Verstappen proved on Sunday that he's in no mood to concede anything to his papaya rivals, pulling off one of the all-time great overtaking moves on pole-sitter Oscar Piastri into Tamburello to take the lead on Lap 1.
-            <br/>
-            That stunning move was the foundation of Verstappen's victory as he raced off at the front, while behind him there was plenty of action – and a surprising amount of overtaking on such a tight track – as the rest of the drivers fought for places.</b>
+          McLaren may well have the fastest overall package this year, but Max Verstappen proved on Sunday that he's in no mood to concede anything to his papaya rivals, pulling off one of the all-time great overtaking moves on pole-sitter Oscar Piastri into Tamburello to take the lead on Lap 1.</p>
+        <br />
+        <p>  That stunning move was the foundation of Verstappen's victory as he raced off at the front, while behind him there was plenty of action and a surprising amount of overtaking on such a tight track  as the rest of the drivers fought for places.
         </p>
 
       </div>
--- src/components/CardSix.jsx
+++ src/components/CardSix.jsx
@@ -4,13 +4,13 @@ export default function CardSix() {
             <h1>
                 What time is the Formula 1 2025 Monaco Grand Prix and how can I watch it?
             </h1>
-            <br/><br/>
-            <img src="../public/img/pic6card6.jpg" style={{width:'1000px'}}/>
+            <br /><br />
+            <img src="../public/img/pic6card6.jpg" style={{ width: '1000px' }} />
 
             <br /><br />
-            <p>Formula 1 moves swiftly on from Imola to Monte Carlo for the Monaco Grand Prix this weekend, as the latest triple header sequence continues.
+            <p>Formula 1 moves swiftly on from Imola to Monte Carlo for the Monaco Grand Prix this weekend, as the latest triple header sequence continues.</p>
 
-                The event will begin with Free Practice 1 and Free Practice 2 on Friday, May 23, followed by Free Practice 3 and Qualifying on Saturday, May 24, and the Grand Prix itself on Sunday, May 2</p>
+            <p>  The event will begin with Free Practice 1 and Free Practice 2 on Friday, May 23, followed by Free Practice 3 and Qualifying on Saturday, May 24, and the Grand Prix itself on Sunday, May 2</p>
         </div>
     )
 }
