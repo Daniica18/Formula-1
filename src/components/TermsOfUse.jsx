e4877f6a9156c62e5f7371a1c1eb6334f999f634
TatjanaStojanovic1701 <tatjana.stojanovic1701@gmail.com>
2025-05-23 14:04:48 +0200

    Fix footer

--- src/components/TermsOfUse.jsx
+++ src/components/TermsOfUse.jsx
@@ -1,14 +1,13 @@
 export default function TermsOfUse() {
   return (
     <>
-      <div>
+      <div className="bottom">
 
         <h1>FORMULA1.COM - TERMS OF USE</h1>
-        <br />
-        <br />
+
         <p>Last Updated: 17/07/2024</p>
         <br />
-        <br />
+
         <p>Formula One Digital Media Limited (registered in England with company number 08915039, whose registered office is at No.2 St. James’s Market, London, SW1Y 4AH) operates www.formula1.com (the "Site") and is part of the Formula 1 group of companies (”Formula 1 Companies”) which have the exclusive right to commercialise the FIA Formula One World Championship in all media.</p>
         <br />
         <p>Access to and use of the Site is provided by Formula One Digital Media Limited ("we", "us", and/or "our") subject to the following terms and conditions.</p>
@@ -22,69 +21,69 @@ export default function TermsOfUse() {
         <p>You are responsible for ensuring that all persons who access this Site through your internet connection are aware of these terms and conditions and other applicable terms and conditions, and that they comply with them.</p>
         <br />
         <h2>Privacy Policy</h2>
-        <br />
+
         <p>We will use any personal data collected through your use of this Site in accordance with our Privacy Policy, which is available at https://account.formula1.com/#/en/privacy-policy.</p>
         <br />
-        <br />
+
         <h2>Other applicable terms</h2>
-        <br />
-        Any subscriptions you purchase from us will be governed by the Subscription Terms, which are available at https://account.formula1.com/#/en/subscription-terms.
+
+        <p> Any subscriptions you purchase from us will be governed by the Subscription Terms, which are available at https://account.formula1.com/#/en/subscription-terms.</p>
         <br />
         <p>We will use cookies on the Site in accordance with our Cookies Policy, which is available at https://account.formula1.com/#/en/cookie-policy/.</p>
         <br />
-        <br />
+
         <h2>Updates And Changes</h2>
         <br />
         <p>We reserve the right to change these terms and conditions at any time by posting changes online or by emailing you. You are responsible for regularly reviewing the terms and conditions posted on this Site ensure you understand the terms that apply at that time. By using this Site after any such changes you agree to comply with, and be bound by, the terms and conditions in force at the time.</p>
         <br />
         <p>We may update and change this Site from time to time in relation to our users’ needs, our business priorities and/or to reflect changes to any product or service offered by us.</p>
         <br />
-        <br />
+
         <h2>Website Access</h2>
-        <br />
+
         <p>You must be 16 years or older to use the Site. For U.S. residents, and residents of any other applicable territory, if you are at least 16 and under 18 years of age (or under the applicable age of majority in your state or territory of residence), you may use an account, but only if you have the consent of your parent or guardian, including consent to these terms and conditions on your behalf, and for clarity, you may only modify an account with the consent of your parent or guardian. Please note that age requirements for purchase of and access to certain Services may vary. Some of the Services will be restricted to residents of certain territories and the content and features included within the Services will differ between territories. For more information about the Services (including the content and features within the Services) available in your territory, please click here.</p>
         <br />
-        <br />
+
         <h2>Availability</h2>
-        <br />
+
         <p>We do not guarantee that the Site, or any content on it, will always be available or be uninterrupted. We may suspend or withdraw or restrict the availability of all or any part of this Site for business and operational reasons (including maintenance, updates, technical improvements or to develop its content and/or presentation).</p>
         <br />
-        <br />
+
         <h2>Viruses, Bugs And Malicious Use</h2>
-        <br />
+
         <p>We and our suppliers take reasonable precautions to prevent computer viruses and any other items that may damage the operation of computers or property or otherwise engage in computer misuse on the Site but cannot accept any liability for them. You are responsible for configuring your information technology, computer programmes, hardware, software and platform to access this Site. You should use your own virus protection software.</p>
         <br />
-        <br />
+
         <p>You must not misuse this Site by introducing viruses, trojans, worms, logic bombs or other material that is malicious or technologically harmful. You must not attempt to gain unauthorised access to this Site, the server on which this Site is stored or any server, computer or database connected to this Site. You must not attack this Site via a denial-of-service attack or a distributed denial-of service attack or doing anything which may affect the functioning of or the ability of any other person to access the Site. By breaching this provision, you would commit a criminal offence under the Computer Misuse Act 1990. We will report any such breach to the relevant law enforcement authorities and we will co-operate with the relevant authorities in their investigation of such incidents. In the event of such a breach, your right to use this Site will cease immediately.</p>
         <br />
-        <br />
+
         <h2>Intellectual Property Rights</h2>
-        <br />
+
         <p>The material and content provided on this Site is for your personal, non-commercial use only (save where expressly provided) and you agree not for yourself or through or by way of assistance from any third party to distribute, copy, extract or commercially exploit such material or content. You further agree not to reproduce, distribute, perform, display, modify, adapt, translate, prepare derivative works from, decompile, reverse engineer, disassemble or otherwise attempt to derive source code from this Site. Except as otherwise indicated, all materials on this Site, including, but not limited to photographs, other images, illustrations, text, video clips and written and other materials contained in this Site are protected by copyrights, database rights, trademarks and/or other intellectual property rights owned, or used with permission of their owners, by us or our affiliates or associates. This Site as a whole is protected by copyright and other intellectual property rights. All rights are reserved.</p>
         <br />
         <p>The F1 FORMULA 1 logo, F1 logo, F1 FIA FORMULA 1 WORLD CHAMPIONSHIP logo, FORMULA 1, FORMULA ONE, F1, FIA FORMULA ONE WORLD CHAMPIONSHIP, GRAND PRIX, F1 GRAND PRIX, FORMULA 1 GRAND PRIX and related marks are trademarks of Formula One Licensing BV, a Formula 1 Company. All rights reserved.</p>
         <br />
         <p>You may make a copy of this content for your personal non-commercial use only, provided that you keep all copyright and other proprietary notices intact and you agree not to modify, copy, reproduce, republish, upload, frame, post, transmit or distribute by any means or in any manner, any material or information on or downloaded from this Site including but not limited to text, graphics, video, messages, code and/or software without our prior written consent. No right, title or interest in any material or software is transferred to you as a result of any such downloading or copying.</p>
         <br />
-        <br />
+
         <h2>Our Liability</h2>
-        <br />
+
         <p>Nothing in these terms and conditions shall exclude our liability for death or personal injury caused by our negligence or the negligence of our employees, agents or subcontractors, for fraud or fraudulent misrepresentation, or for anything else for which it would be unlawful to do so.</p>
         <br />
         <p>To the extent permitted by law, we shall not be liable in contract, tort, negligence, breach of statutory duty, or otherwise for any loss or damage whatsoever in any way connected with your use or inability to use this Site, or your use or reliance on any content on this Site.</p>
         <br />
         <p>Reasonable skill and care has been used in producing the Site but it is only designed for general information purposes. We do not give any guarantee or warranty that the Site or any content thereon (including data or statistics) is accurate, complete or up-to-date. We therefore disclaim all liability and responsibility arising from any reliance placed on the content of the Site by you, or by anyone who may be informed of the Site’s contents.</p>
         <br />
-        <br />
+
         <h2>Third Party Sites</h2>
-        <br />
+
         <p>Certain sections of the Site are operated by third parties, including: (i) the F1 Store (f1store.formula1.com) operated by Fanatics (International) Limited; (ii) Formula 1 Tickets (tickets.formula1.com) operated by Platinium Group S.A.M; and (iii) F1 Experiences (f1experiences.com) operated by QuintEvents International LLC.</p>
         <br />
         <p>You agree that any dealings you have with such third party operators (including payment for and delivery of products or services and any other terms, conditions, warranties or representations, acts or omissions associated with such dealings) shall be on the terms and conditions (if any) of the third party operator and a matter solely between such third party operator and yourself.</p>
         <br />
-        <br />
+
         <h2>Links</h2>
-        <br />
+
         <p>We make no representations whatsoever about any other sites that you may access through this Site or that may link to this Site.</p>
         <br />
         <p>Linking to this Site is permitted so long as such use does not create any unauthorised association between the Formula 1 Companies and its brands, and any third party or its products or services and does not damage our brand or image. The name of this Site must be used in such links (i.e. www.formula1.com) but none of our logos may be used.</p>
@@ -93,9 +92,9 @@ export default function TermsOfUse() {
         <br />
         <p>In addition, a link to any other website does not mean that we endorse or accept any responsibility whatsoever for the content contained on such website or the use by you of it. We shall not be liable for any loss or damage caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available through any such site or resource.</p>
         <br />
-        <br />
+
         <h2>Governing Law</h2>
-        <br />
+
         <p>Except in the case of U.S. residents, these terms of use are governed by English law and you can bring legal proceedings in respect of them in the English courts.</p>
         <br />
         <p>In the case of U.S. residents, these terms governed by and construed in accordance with the laws of the State of New York and the laws of the United States, without giving effect to any conflict-of-law and/or choice-of-law principles.</p>
@@ -103,7 +102,7 @@ export default function TermsOfUse() {
         <p>Regardless of this, you may have statutory and/or consumer rights where you live which apply and your local law may apply in some circumstances.</p>
         <br />
         <h2>Individual Arbitration & Class Action Waiver for U.S. Residents:</h2>
-        <br />
+
         <p>The following terms apply to U.S. residents only:</p>
         <br />
         <p>You agree that any dispute, claim or controversy with us or our agents arising out of or relating in any way to the Site and all other official Formula 1 digital products related to the Site, including but not limited to the F1TV digital subscription products (the “Services”), these Terms, the Subscription Terms, and this Arbitration Agreement, shall be determined by binding arbitration or in small claims court. Except that, nothing in this provision shall prevent us from seeking and obtaining an injunction or other equitable relief from a court in the event of a breach or threatened breach of these terms and conditions.</p>
@@ -123,11 +122,11 @@ export default function TermsOfUse() {
         <p>Your opt-out notice must include your name and address email address you used to transact on the Site (if you have one), and an unequivocal statement that you want to opt out of this Arbitration Agreement. If you opt out of this Arbitration Agreement, all other parts of this Agreement, including the class action waiver, will continue to apply to you, unless precluded by law. Opting out of this Arbitration Agreement has no effect on any other arbitration agreements that you may currently have, or may enter in the future, with us.</p>
         <br />
         <h2>California Consumers Notice</h2>
-        <br />
+
         <p>Under California Civil Code Section 1789.3, California users of the Services are entitled to the following specific consumer rights notice: The Complaint Assistance Unit of the Division of Consumer Services of the California Department of Consumer Affairs may be contacted in writing at 1625 North Market Blvd., Suite N 112, Sacramento CA 95834, or by telephone at (916) 445-1254 or (800) 952-5210.</p>
         <br />
         <h2>Polish Consumers Notice</h2>
-        <br />
+
         <p>Kliknij tutaj, aby wyświetlić wersję w języku polskim: https://account.formula1.com/#/pl/terms-of-use</p>
 
 
