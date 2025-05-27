282bf89407924f66a4676100490e16abae5bba6e
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-27 09:52:55 +0200

    Edit Slider2 image

---
+++ src/components/Slider2.jsx
@@ -0,0 +1,22 @@
+export default function Slider2() {
+    return (
+        <>
+            <div className="Slider2Component">
+                <h1>F1 NATION: Norris back on top as Racing Bulls deliver a strategy masterclass – it’s our Monaco GP review</h1>
+                <br />
+
+                <p> Lando Norris converted pole to victory as he won in Monaco for the first time in his career, with home hero Charles Leclerc second and championship leader Oscar Piastri third. That reignited the intra-team battle at McLaren, with the Australian now just three points ahead of his team mate in the title race.
+                    After the race, two-time Le Mans podium finisher and F1TV analyst, Alex Brundle, and Fox Sports Mexico journalist, Diego Mejia, joined Tom Clarkson in the Monte Carlo paddock to discuss an eventful Monaco Grand Prix in this week's F1 Nation podcast.</p>
+                <br></br>
+                <p>They discuss the turnaround in form for Norris, who has struggled at times in Qualifying and yet delivered on the most important Saturday of the year. But what happened to the unflappable Piastri - why could he suddenly not hook a lap up in Qualifying?
+                    MONACO LOWDOWN: All the key moments as Norris realises a dream, Racing Bulls shine and famous faces enjoy the show
+                    Leclerc delivered on home soil for the second year running as Ferrari enjoyed an upturn in form, somewhat against the odds with the Monegasque sure that he would struggle to even make the top 10 here prior to the weekend.</p>
+                <br />
+
+                <img src="../img/formula1celebrating.jpg" className="imageS2" />
+
+
+            </div>
+        </>
+    )
+}
