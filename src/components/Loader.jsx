62a1f36070f415f112870c1cbc142a5c156c1807
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-27 11:24:09 +0200

    Changed Loader type

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { CircleLoader } from "react-spinners";
+import { SyncLoader } from "react-spinners";
 
 export default class Loader extends React.Component {
     render() {
         return (
             <div className="loader-container">
-                <CircleLoader color="red" size={100} />
+                <SyncLoader color="red" size={60} />
             </div>
         );
     }
