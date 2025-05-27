6d2424ce00a638ea291d03474285869ebe0dab92
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-27 13:28:33 +0200

    Fix CardData

--- src/CardData.jsx
+++ src/CardData.jsx
@@ -14,7 +14,7 @@ Our experts’ scores are then averaged out to produce a race score – with tho
     {
         id: 1,
         title: "Vasseur admits Ferrari are not fast enough as he highlights Qualifying concerns ahead of Monaco",
-        image: "../img/card2.jpg",
+        image: "../img/Card2.jpg",
         description: `For many, Ferrari and Formula 1 racing have become inseparable. The only team to have competed in every season since the world championship began, the Prancing Horse has grown from the humble dream of founder Enzo Ferrari to become one of the most iconic and recognised brands in the world.
            Success came quickly through the likes of Alberto Ascari and John Surtees, and continued – in amongst leaner times – with Niki Lauda in the 1970s and then Michael Schumacher in the 2000s, when Ferrari claimed a then unprecedented five consecutive title doubles, securing their status as the most successful and decorated team in F1 history....Team Principal Fred Vasseur’s leadership starts to gel and strong campaign sees Charles Leclerc and Carlos Sainz take five wins between them. Runners-up in the constructors’ standings, losing out to McLaren at the final round. Lewis Hamilton signed to replace Sainz for 2025.`
     },
