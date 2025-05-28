3f36977c3c536ee25b3158999b54659ee1d059c2
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-28 09:36:01 +0200

    Add live track in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ import SliderInfo from './SliderInfo';
 
 export default function Home() {
 
-    
+
 
     return (
         <div>
@@ -35,6 +35,33 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-        </div>
+
+            {/* LIVE */}
+            <div className="live-wrapper">
+                <div className="live">
+                    <span className="live-content">
+                        🏁🏁🏁 Watch all F1 sessions live with F1 TV. Live Formula 1 Race: Watch now on&nbsp;
+                        <Link to="https://www.formula1.com/en-rs/subscribe-to-f1-tv?from=us&to=rs"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="live-link"
+                        >
+                            LIVE STREAM
+                        </Link>
+                    </span>
+                    <span className="live-content">
+                        🏁🏁🏁 Watch all F1 sessions live with F1 TV. Live Formula 1 Race: Watch now on&nbsp;
+                        <Link to="https://www.formula1.com/en-rs/subscribe-to-f1-tv?from=us&to=rs"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="live-link"
+                        >
+                            LIVE STREAM
+                        </Link>
+                    </span>
+
+                </div>
+            </div>
+        </div >
     );
 }
