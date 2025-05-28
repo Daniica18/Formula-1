9c6ec47141dd3352c11224acc14e5f90b6357f3a
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-28 15:26:02 +0200

    Added new divs in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export default function Footer() {
     const year = (new Date()).getFullYear();
 
     return (
         <footer className="footer_div">
+            <div className="marketing">
+                <div className="appPhoto">
+                    <p>DOWNLOAD THE OFFICIAL F1 APP</p>
+                    <img src="./img/GooglePlay.jpg" alt="" />
+                    <img src="./img/AppStore.jpg" alt="" />
+                </div>
+                <div className="appLink">
+                    <FacebookIcon />
+                    <InstagramIcon />
+                    <XIcon />
+                    <YouTubeIcon />
+                </div>
+            </div>
             <div className="footer">
                 <Link to="/Contact">Contact</Link>
                 <Link to="/PrivacyPolicy">Privacy Policy</Link>
