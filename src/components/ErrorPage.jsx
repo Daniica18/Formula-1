045f1c84c39b1270cbc1046669df156ed35b9dac
Danica Svorcan <danicasvorcan18@gmail.com>
2025-05-29 11:47:35 +0200

    Modified ErrorPage and added design

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,12 @@
 export default function ErrorPage() {
     return (
-        <>
-            <h1>There has been an error with loading data</h1>
-        </>
+        <div className='errorPage'>
+            <div>
+                <h1>😔Oops!</h1>
+            </div>
+            <div>
+                <h2>ERROR! There has been an error with loading data</h2>
+            </div>
+        </div>
     )
 }
